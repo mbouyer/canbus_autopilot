834a401d5c32f4ecda142a2614aa4e5b3a0eb389
Manuel Bouyer <bouyer@antioche.eu.org>
2019-03-18 00:11:05 +0100

    Switch to use static reserved memory, avoids saving registers and multiple banksel. Saves about 15ms per round, or more than 16%.

--- software/capteur/fsadd.s
+++ software/capteur/fsadd.s
@@ -31,28 +31,23 @@ PLUSW2	equ	0xfdb
 PRODL	equ	0xff3
 PRODH	equ	0xff4
 
+.privatefp	udata
 
-; Internal registers
-.registers	udata_ovr	0x0000
-r0x00	res	1
-r0x01	res	1
-r0x02	res	1
-r0x03	res	1
-r0x04	res	1
-r0x05	res	1
-r0x06	res	1
-r0x07	res	1
-r0x0b	res	1
-r0x0c	res	1
-r0x0d	res	1
-r0x0e	res	1
-r0x0f	res	1
-
-udata_fsadd_0	udata
-___fsadd_fl1_1_22	res	4
-
-udata_fsadd_1	udata
-___fsadd_fl2_1_22	res	4
+r1x00	res	1
+r1x01	res	1
+r1x02	res	1
+r1x03	res	1
+r1x04	res	1
+r1x05	res	1
+r1x06	res	1
+r1x07	res	1
+r1x0b	res	1
+r1x0c	res	1
+r1x0d	res	1
+r1x0e	res	1
+r1x0f	res	1
+___fsxxx_fl1	res	4
+___fsxxx_fl2	res	4
 
 ;--------------------------------------------------------
 ; global & static initialisations
@@ -64,390 +59,353 @@ ___fsadd:
 ;	.line	54; fsadd.c	__fsadd (float a1, float a2) _FS_REENTRANT
 	MOVFF	FSR2L, POSTDEC1
 	MOVFF	FSR1L, FSR2L
-	MOVFF	r0x00, POSTDEC1
-	MOVFF	r0x01, POSTDEC1
-	MOVFF	r0x02, POSTDEC1
-	MOVFF	r0x03, POSTDEC1
-	MOVFF	r0x04, POSTDEC1
-	MOVFF	r0x05, POSTDEC1
-	MOVFF	r0x06, POSTDEC1
-	MOVFF	r0x07, POSTDEC1
-	MOVFF	r0x0b, POSTDEC1
-	MOVFF	r0x0c, POSTDEC1
-	MOVFF	r0x0d, POSTDEC1
-	MOVFF	r0x0e, POSTDEC1
-	MOVFF	r0x0f, POSTDEC1
+	BANKSEL r1x00
 ;	.line	61; fsadd.c	fl1.f = a1;
 	MOVLW	0x02
-	MOVFF	PLUSW2, ___fsadd_fl1_1_22
+	MOVFF	PLUSW2, ___fsxxx_fl1
 	MOVLW	0x03
-	MOVFF	PLUSW2, (___fsadd_fl1_1_22 + 1)
+	MOVFF	PLUSW2, (___fsxxx_fl1 + 1)
 	MOVLW	0x04
-	MOVFF	PLUSW2, (___fsadd_fl1_1_22 + 2)
+	MOVFF	PLUSW2, (___fsxxx_fl1 + 2)
 	MOVLW	0x05
-	MOVFF	PLUSW2, (___fsadd_fl1_1_22 + 3)
+	MOVFF	PLUSW2, (___fsxxx_fl1 + 3)
 ;	.line	62; fsadd.c	fl2.f = a2;
 	MOVLW	0x06
-	MOVFF	PLUSW2, ___fsadd_fl2_1_22
+	MOVFF	PLUSW2, ___fsxxx_fl2
 	MOVLW	0x07
-	MOVFF	PLUSW2, (___fsadd_fl2_1_22 + 1)
+	MOVFF	PLUSW2, (___fsxxx_fl2 + 1)
 	MOVLW	0x08
-	MOVFF	PLUSW2, (___fsadd_fl2_1_22 + 2)
+	MOVFF	PLUSW2, (___fsxxx_fl2 + 2)
 	MOVLW	0x09
-	MOVFF	PLUSW2, (___fsadd_fl2_1_22 + 3)
+	MOVFF	PLUSW2, (___fsxxx_fl2 + 3)
 ;	.line	59; fsadd.c	unsigned long sign = 0;
-	CLRF	r0x0b,   A
-	BANKSEL	___fsadd_fl1_1_22
+	CLRF	r1x0b, B
 ;	.line	65; fsadd.c	if (!fl1.l)
-	MOVF	___fsadd_fl1_1_22, W, B
-	IORWF	(___fsadd_fl1_1_22 + 1), W, B
-	IORWF	(___fsadd_fl1_1_22 + 2), W, B
-	IORWF	(___fsadd_fl1_1_22 + 3), W, B
+	MOVF	___fsxxx_fl1, W, B
+	IORWF	(___fsxxx_fl1 + 1), W, B
+	IORWF	(___fsxxx_fl1 + 2), W, B
+	IORWF	(___fsxxx_fl1 + 3), W, B
 	BNZ	_00106_DS_
 ;	.line	66; fsadd.c	return fl2.f;
-	MOVFF	(___fsadd_fl2_1_22 + 3), FSR0L
-	MOVFF	(___fsadd_fl2_1_22 + 2), PRODH
-	MOVFF	(___fsadd_fl2_1_22 + 1), PRODL
-	BANKSEL	___fsadd_fl2_1_22
-	MOVF	___fsadd_fl2_1_22, W, B
+	MOVFF	(___fsxxx_fl2 + 3), FSR0L
+	MOVFF	(___fsxxx_fl2 + 2), PRODH
+	MOVFF	(___fsxxx_fl2 + 1), PRODL
+	MOVF	___fsxxx_fl2, W, B
 	BRA	_00133_DS_
 _00106_DS_:
-	BANKSEL	___fsadd_fl2_1_22
 ;	.line	67; fsadd.c	if (!fl2.l)
-	MOVF	___fsadd_fl2_1_22, W, B
-	IORWF	(___fsadd_fl2_1_22 + 1), W, B
-	IORWF	(___fsadd_fl2_1_22 + 2), W, B
-	IORWF	(___fsadd_fl2_1_22 + 3), W, B
+	MOVF	___fsxxx_fl2, W, B
+	IORWF	(___fsxxx_fl2 + 1), W, B
+	IORWF	(___fsxxx_fl2 + 2), W, B
+	IORWF	(___fsxxx_fl2 + 3), W, B
 	BNZ	_00108_DS_
 ;	.line	68; fsadd.c	return fl1.f;
-	MOVFF	(___fsadd_fl1_1_22 + 3), FSR0L
-	MOVFF	(___fsadd_fl1_1_22 + 2), PRODH
-	MOVFF	(___fsadd_fl1_1_22 + 1), PRODL
-	BANKSEL	___fsadd_fl1_1_22
-	MOVF	___fsadd_fl1_1_22, W, B
+	MOVFF	(___fsxxx_fl1 + 3), FSR0L
+	MOVFF	(___fsxxx_fl1 + 2), PRODH
+	MOVFF	(___fsxxx_fl1 + 1), PRODL
+	MOVF	___fsxxx_fl1, W, B
 	BRA	_00133_DS_
 _00108_DS_:
-	BANKSEL	(___fsadd_fl1_1_22 + 2)
 ;	.line	70; fsadd.c	exp1 = EXP (fl1.l);
-	RLCF	(___fsadd_fl1_1_22 + 2), W, B
-	RLCF	(___fsadd_fl1_1_22 + 3), W, B
-	MOVWF	r0x00,   A
-	CLRF	r0x01,   A
-	CLRF	r0x02,   A
-	CLRF	r0x03,   A
-	BANKSEL	(___fsadd_fl2_1_22 + 2)
+	RLCF	(___fsxxx_fl1 + 2), W, B
+	RLCF	(___fsxxx_fl1 + 3), W, B
+	MOVWF	r1x00, B
+	CLRF	r1x01, B
+	CLRF	r1x02, B
+	CLRF	r1x03, B
 ;	.line	71; fsadd.c	exp2 = EXP (fl2.l);
-	RLCF	(___fsadd_fl2_1_22 + 2), W, B
-	RLCF	(___fsadd_fl2_1_22 + 3), W, B
-	MOVWF	r0x02,   A
+	RLCF	(___fsxxx_fl2 + 2), W, B
+	RLCF	(___fsxxx_fl2 + 3), W, B
+	MOVWF	r1x02, B
 ;	.line	73; fsadd.c	if (exp1 > exp2 + 25)
 	ADDLW	0x19
-	MOVWF	r0x04,   A
+	MOVWF	r1x04, B
 	MOVLW	0x00
-	ADDWFC	r0x03, W
-; #	MOVWF	r0x05,   A
-; #	MOVF	r0x05, W
+	ADDWFC	r1x03, W, B
+; #	MOVWF	r1x05, B
+; #	MOVF	r1x05, W
 	ADDLW	0x80
-	MOVWF	PRODL,   A
-	MOVF	r0x01, W
+	MOVWF	PRODL
+	MOVF	r1x01, W
 	ADDLW	0x80
-	SUBWF	PRODL, W
+	SUBWF	PRODL
 	BNZ	_00181_DS_
-	MOVF	r0x00, W
-	SUBWF	r0x04, W
+	MOVF	r1x00, W, B
+	SUBWF	r1x04, W, B
 _00181_DS_:
 	BC	_00110_DS_
 ;	.line	74; fsadd.c	return fl1.f;
-	MOVFF	(___fsadd_fl1_1_22 + 3), FSR0L
-	MOVFF	(___fsadd_fl1_1_22 + 2), PRODH
-	MOVFF	(___fsadd_fl1_1_22 + 1), PRODL
-	BANKSEL	___fsadd_fl1_1_22
-	MOVF	___fsadd_fl1_1_22, W, B
+	MOVFF	(___fsxxx_fl1 + 3), FSR0L
+	MOVFF	(___fsxxx_fl1 + 2), PRODH
+	MOVFF	(___fsxxx_fl1 + 1), PRODL
+	MOVF	___fsxxx_fl1, W, B
 	BRA	_00133_DS_
 _00110_DS_:
 ;	.line	75; fsadd.c	if (exp2 > exp1 + 25)
-	MOVF	r0x00, W
+	MOVF	r1x00, W, B
 	ADDLW	0x19
-	MOVWF	r0x04,   A
+	MOVWF	r1x04, B
 	MOVLW	0x00
-	ADDWFC	r0x01, W
-; #	MOVWF	r0x05,   A
-; #	MOVF	r0x05, W
+	ADDWFC	r1x01, W, B
+; #	MOVWF	r1x05, B
+; #	MOVF	r1x05, W
 	ADDLW	0x80
-	MOVWF	PRODL,   A
-	MOVF	r0x03, W
+	MOVWF	PRODL
+	MOVF	r1x03, W, B
 	ADDLW	0x80
 	SUBWF	PRODL, W
 	BNZ	_00182_DS_
-	MOVF	r0x02, W
-	SUBWF	r0x04, W
+	MOVF	r1x02, W, B
+	SUBWF	r1x04, W, B
 _00182_DS_:
 	BC	_00112_DS_
 ;	.line	76; fsadd.c	return fl2.f;
-	MOVFF	(___fsadd_fl2_1_22 + 3), FSR0L
-	MOVFF	(___fsadd_fl2_1_22 + 2), PRODH
-	MOVFF	(___fsadd_fl2_1_22 + 1), PRODL
-	BANKSEL	___fsadd_fl2_1_22
-	MOVF	___fsadd_fl2_1_22, W, B
+	MOVFF	(___fsxxx_fl2 + 3), FSR0L
+	MOVFF	(___fsxxx_fl2 + 2), PRODH
+	MOVFF	(___fsxxx_fl2 + 1), PRODL
+	MOVF	___fsxxx_fl2, W, B
 	BRA	_00133_DS_
 _00112_DS_:
-	BANKSEL	___fsadd_fl1_1_22
 ;	.line	78; fsadd.c	mant1 = MANT (fl1.l);
-	MOVF	___fsadd_fl1_1_22, W, B
-	MOVWF	r0x04,   A
-	MOVF	(___fsadd_fl1_1_22 + 1), W, B
-	MOVWF	r0x05,   A
-	MOVF	(___fsadd_fl1_1_22 + 2), W, B
-	MOVWF	r0x06,   A
-	CLRF	r0x07,   A
-	BSF	r0x06, 7
-	BANKSEL	___fsadd_fl2_1_22
+	MOVF	___fsxxx_fl1, W, B
+	MOVWF	r1x04, B
+	MOVF	(___fsxxx_fl1 + 1), W, B
+	MOVWF	r1x05, B
+	MOVF	(___fsxxx_fl1 + 2), W, B
+	MOVWF	r1x06, B
+	CLRF	r1x07, B
+	BSF	r1x06, 7, B
 ;	.line	79; fsadd.c	mant2 = MANT (fl2.l);
-	MOVF	___fsadd_fl2_1_22, W, B
-	MOVWF	r0x0c,   A
-	MOVF	(___fsadd_fl2_1_22 + 1), W, B
-	MOVWF	r0x0d,   A
-	MOVF	(___fsadd_fl2_1_22 + 2), W, B
-	MOVWF	r0x0e,   A
-	CLRF	r0x0f,   A
-	BSF	r0x0e, 7
-	BANKSEL	(___fsadd_fl1_1_22 + 3)
+	MOVF	___fsxxx_fl2, W, B
+	MOVWF	r1x0c, B
+	MOVF	(___fsxxx_fl2 + 1), W, B
+	MOVWF	r1x0d, B
+	MOVF	(___fsxxx_fl2 + 2), W, B
+	MOVWF	r1x0e, B
+	CLRF	r1x0f, B
+	BSF	r1x0e, 7, B
 ;	.line	81; fsadd.c	if (SIGN (fl1.l))
-	MOVF	(___fsadd_fl1_1_22 + 3), W, B
+	MOVF	(___fsxxx_fl1 + 3), W, B
 	ANDLW	0x80
 	BZ	_00114_DS_
 ;	.line	82; fsadd.c	mant1 = -mant1;
-	COMF	r0x07, F
-	COMF	r0x06, F
-	COMF	r0x05, F
-	NEGF	r0x04,   A
+	COMF	r1x07, F, B
+	COMF	r1x06, F, B
+	COMF	r1x05, F, B
+	NEGF	r1x04, B
 	BNZ	_00114_DS_
-	INCF	r0x05, F
+	INCF	r1x05, F, B
 	BNZ	_00114_DS_
-	INCF	r0x06, F
+	INCF	r1x06, F, B
 	BTFSC	STATUS, 2
-	INCF	r0x07, F
+	INCF	r1x07, F, B
 _00114_DS_:
-	BANKSEL	(___fsadd_fl2_1_22 + 3)
 ;	.line	83; fsadd.c	if (SIGN (fl2.l))
-	MOVF	(___fsadd_fl2_1_22 + 3), W, B
+	MOVF	(___fsxxx_fl2 + 3), W, B
 	ANDLW	0x80
 	BZ	_00116_DS_
 ;	.line	84; fsadd.c	mant2 = -mant2;
-	COMF	r0x0f, F
-	COMF	r0x0e, F
-	COMF	r0x0d, F
-	NEGF	r0x0c,   A
+	COMF	r1x0f, F, B
+	COMF	r1x0e, F, B
+	COMF	r1x0d, F, B
+	NEGF	r1x0c, B
 	BNZ	_00116_DS_
-	INCF	r0x0d, F
+	INCF	r1x0d, F, B
 	BNZ	_00116_DS_
-	INCF	r0x0e, F
+	INCF	r1x0e, F, B
 	BTFSC	STATUS, 2
-	INCF	r0x0f, F
+	INCF	r1x0f, F, B
 _00116_DS_:
 ;	.line	86; fsadd.c	if (exp1 > exp2)
-	MOVF	r0x03, W
+	MOVF	r1x03, W, B
 	ADDLW	0x80
-	MOVWF	PRODL,   A
-	MOVF	r0x01, W
+	MOVWF	PRODL
+	MOVF	r1x01, W, B
 	ADDLW	0x80
 	SUBWF	PRODL, W
 	BNZ	_00189_DS_
-	MOVF	r0x00, W
-	SUBWF	r0x02, W
+	MOVF	r1x00, W, B
+	SUBWF	r1x02, W, B
 _00189_DS_:
 	BC	_00118_DS_
 ;	.line	88; fsadd.c	mant2 >>= exp1 - exp2;
-	MOVF	r0x02, W
-	SUBWF	r0x00, W
+	MOVF	r1x02, W, B
+	SUBWF	r1x00, W, B
 	BZ	_00119_DS_
 	BN	_00193_DS_
-	NEGF	WREG,   A
+	NEGF	WREG
 	BCF	STATUS, 0
 _00191_DS_:
-	BTFSC	r0x0f, 7
+	BTFSC	r1x0f, 7, B
 	BSF	STATUS, 0
-	RRCF	r0x0f, F
-	RRCF	r0x0e, F
-	RRCF	r0x0d, F
-	RRCF	r0x0c, F
+	RRCF	r1x0f, F, B
+	RRCF	r1x0e, F, B
+	RRCF	r1x0d, F, B
+	RRCF	r1x0c, F, B
 	ADDLW	0x01
 	BNC	_00191_DS_
 	BRA	_00119_DS_
 _00193_DS_:
 	BCF	STATUS, 0
 _00192_DS_:
-	RLCF	r0x0c, F
-	RLCF	r0x0d, F
-	RLCF	r0x0e, F
-	RLCF	r0x0f, F
+	RLCF	r1x0c, F, B
+	RLCF	r1x0d, F, B
+	RLCF	r1x0e, F, B
+	RLCF	r1x0f, F, B
 	ADDLW	0x01
 	BNC	_00192_DS_
 _00190_DS_:
 	BRA	_00119_DS_
 _00118_DS_:
 ;	.line	92; fsadd.c	mant1 >>= exp2 - exp1;
-	MOVF	r0x00, W
-	SUBWF	r0x02, W
+	MOVF	r1x00, W, B
+	SUBWF	r1x02, W, B
 	BZ	_00194_DS_
 	BN	_00197_DS_
-	NEGF	WREG,   A
+	NEGF	WREG
 	BCF	STATUS, 0
 _00195_DS_:
-	BTFSC	r0x07, 7
+	BTFSC	r1x07, 7, B
 	BSF	STATUS, 0
-	RRCF	r0x07, F
-	RRCF	r0x06, F
-	RRCF	r0x05, F
-	RRCF	r0x04, F
+	RRCF	r1x07, F, B
+	RRCF	r1x06, F, B
+	RRCF	r1x05, F, B
+	RRCF	r1x04, F, B
 	ADDLW	0x01
 	BNC	_00195_DS_
 	BRA	_00194_DS_
 _00197_DS_:
 	BCF	STATUS, 0
 _00196_DS_:
-	RLCF	r0x04, F
-	RLCF	r0x05, F
-	RLCF	r0x06, F
-	RLCF	r0x07, F
+	RLCF	r1x04, F, B
+	RLCF	r1x05, F, B
+	RLCF	r1x06, F, B
+	RLCF	r1x07, F, B
 	ADDLW	0x01
 	BNC	_00196_DS_
 _00194_DS_:
 ;	.line	93; fsadd.c	exp1 = exp2;
-	MOVFF	r0x02, r0x00
-	MOVFF	r0x03, r0x01
+	MOVFF	r1x02, r1x00
+	MOVFF	r1x03, r1x01
 _00119_DS_:
 ;	.line	95; fsadd.c	mant1 += mant2;
-	MOVF	r0x0c, W
-	ADDWF	r0x04, F
-	MOVF	r0x0d, W
-	ADDWFC	r0x05, F
-	MOVF	r0x0e, W
-	ADDWFC	r0x06, F
-	MOVF	r0x0f, W
-	ADDWFC	r0x07, F
+	MOVF	r1x0c, W, B
+	ADDWF	r1x04, F, B
+	MOVF	r1x0d, W, B
+	ADDWFC	r1x05, F, B
+	MOVF	r1x0e, W, B
+	ADDWFC	r1x06, F, B
+	MOVF	r1x0f, W, B
+	ADDWFC	r1x07, F, B
 ;	.line	97; fsadd.c	if (mant1 < 0)
-	BTFSS	r0x07, 7
+	BTFSS	r1x07, 7, B
 	BRA	_00123_DS_
 ;	.line	99; fsadd.c	mant1 = -mant1;
-	COMF	r0x07, F
-	COMF	r0x06, F
-	COMF	r0x05, F
-	NEGF	r0x04,   A
+	COMF	r1x07, F, B
+	COMF	r1x06, F, B
+	COMF	r1x05, F, B
+	NEGF	r1x04, B
 	BNZ	_00198_DS_
-	INCF	r0x05, F
+	INCF	r1x05, F, B
 	BNZ	_00198_DS_
-	INCF	r0x06, F
+	INCF	r1x06, F, B
 	BTFSC	STATUS, 2
-	INCF	r0x07, F
+	INCF	r1x07, F, B
 _00198_DS_:
 ;	.line	100; fsadd.c	sign = SIGNBIT;
 	MOVLW	0x80
-	MOVWF	r0x0b,   A
+	MOVWF	r1x0b, B
 	BRA	_00125_DS_
 _00123_DS_:
 ;	.line	102; fsadd.c	else if (!mant1)
-	MOVF	r0x04, W
-	IORWF	r0x05, W
-	IORWF	r0x06, W
-	IORWF	r0x07, W
+	MOVF	r1x04, W, B
+	IORWF	r1x05, W, B
+	IORWF	r1x06, W, B
+	IORWF	r1x07, W, B
 	BNZ	_00125_DS_
 ;	.line	103; fsadd.c	return (0);
-	CLRF	FSR0L,   A
-	CLRF	PRODH,   A
-	CLRF	PRODL,   A
-	CLRF	WREG,   A
+	CLRF	FSR0L
+	CLRF	PRODH
+	CLRF	PRODL
+	CLRF	WREG
 	BRA	_00133_DS_
 _00125_DS_:
 ;	.line	106; fsadd.c	while (mant1 < HIDDEN)
 	MOVLW	0x00
-	SUBWF	r0x07, W
+	SUBWF	r1x07, W, B
 	BNZ	_00199_DS_
 	MOVLW	0x80
-	SUBWF	r0x06, W
+	SUBWF	r1x06, W, B
 	BNZ	_00199_DS_
 	MOVLW	0x00
-	SUBWF	r0x05, W
+	SUBWF	r1x05, W, B
 	BNZ	_00199_DS_
 	MOVLW	0x00
-	SUBWF	r0x04, W
+	SUBWF	r1x04, W, B
 _00199_DS_:
 	BC	_00130_DS_
 ;	.line	108; fsadd.c	mant1 <<= 1;
-	MOVF	r0x04, W
-	ADDWF	r0x04, F
-	RLCF	r0x05, F
-	RLCF	r0x06, F
-	RLCF	r0x07, F
+	MOVF	r1x04, W, B
+	ADDWF	r1x04, F, B
+	RLCF	r1x05, F, B
+	RLCF	r1x06, F, B
+	RLCF	r1x07, F, B
 ;	.line	109; fsadd.c	--exp1;
 	MOVLW	0xff
-	ADDWF	r0x00, F
-	ADDWFC	r0x01, F
+	ADDWF	r1x00, F, B
+	ADDWFC	r1x01, F, B
 	BRA	_00125_DS_
 _00130_DS_:
 ;	.line	113; fsadd.c	while (mant1 & 0xff000000)
-	MOVF	r0x07, W
+	MOVF	r1x07, W, B
 	BZ	_00132_DS_
 ;	.line	115; fsadd.c	if (mant1&1)
-	BTFSS	r0x04, 0
+	BTFSS	r1x04, 0, B
 	BRA	_00129_DS_
 ;	.line	116; fsadd.c	mant1 += 2;
 	MOVLW	0x02
-	ADDWF	r0x04, F
+	ADDWF	r1x04, F, B
 	MOVLW	0x00
-	ADDWFC	r0x05, F
-	ADDWFC	r0x06, F
-	ADDWFC	r0x07, F
+	ADDWFC	r1x05, F, B
+	ADDWFC	r1x06, F, B
+	ADDWFC	r1x07, F, B
 _00129_DS_:
 ;	.line	117; fsadd.c	mant1 >>= 1 ;
-	RLCF	r0x07, W
-	RRCF	r0x07, F
-	RRCF	r0x06, F
-	RRCF	r0x05, F
-	RRCF	r0x04, F
+	RLCF	r1x07, W, B
+	RRCF	r1x07, F, B
+	RRCF	r1x06, F, B
+	RRCF	r1x05, F, B
+	RRCF	r1x04, F, B
 ;	.line	118; fsadd.c	exp1++;
-	INFSNZ	r0x00, F
-	INCF	r0x01, F
+	INFSNZ	r1x00, F, B
+	INCF	r1x01, F, B
 	BRA	_00130_DS_
 _00132_DS_:
 ;	.line	122; fsadd.c	mant1 &= ~HIDDEN;
 	MOVLW	0x7f
-	ANDWF	r0x06, F
+	ANDWF	r1x06, F, B
 ;	.line	125; fsadd.c	fl1.l = PACK (sign, (unsigned long) exp1, mant1);
-	CLRF	WREG,   A
-	RRCF	r0x01, W
-	RRCF	r0x00, W
-	MOVWF	r0x0f,   A
-	CLRF	r0x0e,   A
-	RRCF	r0x0e, F
-	MOVF	r0x0b, W
-	IORWF	r0x0f, F
-	MOVF	r0x06, W
-	IORWF	r0x0e, F
-	MOVF	r0x07, W
-	IORWF	r0x0f, F
+	CLRF	WREG
+	RRCF	r1x01, W, B
+	RRCF	r1x00, W, B
+	MOVWF	r1x0f, B
+	CLRF	r1x0e, B
+	RRCF	r1x0e, F, B
+	MOVF	r1x0b, W, B
+	IORWF	r1x0f, F, B
+	MOVF	r1x06, W, B
+	IORWF	r1x0e, F, B
+	MOVF	r1x07, W, B
+	IORWF	r1x0f, F, B
 ;	.line	127; fsadd.c	return fl1.f;
-	MOVF	r0x0f, W
-	MOVWF	FSR0L,   A
-	MOVF	r0x0e, W
-	MOVWF	PRODH,   A
-	MOVF	r0x05, W
-	MOVWF	PRODL,   A
-	MOVF	r0x04, W
+	MOVF	r1x0f, W, B
+	MOVWF	FSR0L
+	MOVF	r1x0e, W, B
+	MOVWF	PRODH
+	MOVF	r1x05, W, B
+	MOVWF	PRODL
+	MOVF	r1x04, W, B
 _00133_DS_:
-	MOVFF	PREINC1, r0x0f
-	MOVFF	PREINC1, r0x0e
-	MOVFF	PREINC1, r0x0d
-	MOVFF	PREINC1, r0x0c
-	MOVFF	PREINC1, r0x0b
-	MOVFF	PREINC1, r0x07
-	MOVFF	PREINC1, r0x06
-	MOVFF	PREINC1, r0x05
-	MOVFF	PREINC1, r0x04
-	MOVFF	PREINC1, r0x03
-	MOVFF	PREINC1, r0x02
-	MOVFF	PREINC1, r0x01
-	MOVFF	PREINC1, r0x00
 	MOVFF	PREINC1, FSR2L
 	RETURN	
 
--- software/capteur/fsmul.s
+++ software/capteur/fsmul.s
@@ -37,23 +37,26 @@ PRODL	equ	0xff3
 PRODH	equ	0xff4
 
 
-; Internal registers
-.registers	udata_ovr	0x0000
-r0x00	res	1
-r0x01	res	1
-r0x02	res	1
-r0x03	res	1
-r0x04	res	1
-r0x05	res	1
-r0x06	res	1
-r0x10	res	1
-r0x11	res	1
-r0x12	res	1
-r0x13	res	1
-r0x14	res	1
-r0x15	res	1
-r0x16	res	1
-r0x17	res	1
+.privatefp      udata
+
+r1x00   res     1
+r1x01   res     1
+r1x02   res     1
+r1x03   res     1
+r1x04   res     1
+r1x05   res     1
+r1x06   res     1
+r1x10   res     1
+r1x11   res     1
+r1x12   res     1
+r1x13   res     1
+r1x14   res     1
+r1x15   res     1
+r1x16   res     1
+r1x17   res     1
+
+
+
 
 ;--------------------------------------------------------
 ; global & static initialisations
@@ -65,49 +68,35 @@ ___fsmul:
 ;	.line	56; fsmul.c	__fsmul (float a1, float a2) _FS_REENTRANT
 	MOVFF	FSR2L, POSTDEC1
 	MOVFF	FSR1L, FSR2L
-	MOVFF	r0x00, POSTDEC1
-	MOVFF	r0x01, POSTDEC1
-	MOVFF	r0x02, POSTDEC1
-	MOVFF	r0x03, POSTDEC1
-	MOVFF	r0x04, POSTDEC1
-	MOVFF	r0x05, POSTDEC1
-	MOVFF	r0x06, POSTDEC1
-	MOVFF	r0x10, POSTDEC1
-	MOVFF	r0x11, POSTDEC1
-	MOVFF	r0x12, POSTDEC1
-	MOVFF	r0x13, POSTDEC1
-	MOVFF	r0x14, POSTDEC1
-	MOVFF	r0x15, POSTDEC1
-	MOVFF	r0x16, POSTDEC1
-	MOVFF	r0x17, POSTDEC1
+	BANKSEL r1x00
 ;	.line	63; fsmul.c	fl1.f = a1;
 	MOVLW	0x02
-	MOVFF	PLUSW2, r0x10
+	MOVFF	PLUSW2, r1x10
 	MOVLW	0x03
-	MOVFF	PLUSW2, r0x11
+	MOVFF	PLUSW2, r1x11
 	MOVLW	0x04
-	MOVFF	PLUSW2, r0x12
+	MOVFF	PLUSW2, r1x12
 	MOVLW	0x05
-	MOVFF	PLUSW2, r0x13
+	MOVFF	PLUSW2, r1x13
 ;	.line	64; fsmul.c	fl2.f = a2;
 	MOVLW	0x06
-	MOVFF	PLUSW2, r0x14
+	MOVFF	PLUSW2, r1x14
 	MOVLW	0x07
-	MOVFF	PLUSW2, r0x15
+	MOVFF	PLUSW2, r1x15
 	MOVLW	0x08
-	MOVFF	PLUSW2, r0x16
+	MOVFF	PLUSW2, r1x16
 	MOVLW	0x09
-	MOVFF	PLUSW2, r0x17
+	MOVFF	PLUSW2, r1x17
 ;	.line	66; fsmul.c	if (!fl1.l || !fl2.l)
-	MOVF	r0x10, W
-	IORWF	(r0x10 + 1), W
-	IORWF	(r0x10 + 2), W
-	IORWF	(r0x10 + 3), W
+	MOVF	r1x10, W, B
+	IORWF	(r1x10 + 1), W, B
+	IORWF	(r1x10 + 2), W, B
+	IORWF	(r1x10 + 3), W, B
 	BZ	_00105_DS_
-	MOVF	r0x14, W
-	IORWF	(r0x14 + 1), W
-	IORWF	(r0x14 + 2), W
-	IORWF	(r0x14 + 3), W
+	MOVF	r1x14, W, B
+	IORWF	(r1x14 + 1), W, B
+	IORWF	(r1x14 + 2), W, B
+	IORWF	(r1x14 + 3), W, B
 	BNZ	_00106_DS_
 _00105_DS_:
 ;	.line	67; fsmul.c	return 0;
@@ -118,166 +107,169 @@ _00105_DS_:
 	BRA	_00117_DS_
 _00106_DS_:
 ;	.line	70; fsmul.c	sign = SIGN (fl1.l) ^ SIGN (fl2.l);
-	MOVF	(r0x10 + 3), W
+	MOVF	(r1x10 + 3), W, B
 	ANDLW	0x80
 	RLNCF	WREG, W
-	MOVWF	r0x00,   A
-	MOVF	(r0x14 + 3), W
+	MOVWF	r1x00,   B
+	MOVF	(r1x14 + 3), W, B
 	ANDLW	0x80
 	RLNCF	WREG, W
-	XORWF	r0x00, F
+	XORWF	r1x00, F, B
 ;	.line	71; fsmul.c	exp = EXP (fl1.l) - EXCESS;
-	RLCF	(r0x10 + 2), W
-	RLCF	(r0x10 + 3), W
-	MOVWF	r0x01,   A
-	CLRF	r0x02,   A
+	RLCF	(r1x10 + 2), W, B
+	RLCF	(r1x10 + 3), W, B
+	MOVWF	r1x01,   B
+	CLRF	r1x02,   B
 	MOVLW	0x82
-	ADDWF	r0x01, F
+	ADDWF	r1x01, F, B
 	MOVLW	0xff
-	ADDWFC	r0x02, F
+	ADDWFC	r1x02, F, B
 ;	.line	72; fsmul.c	exp += EXP (fl2.l);
-	CLRF	r0x04,   A
-	RLCF	(r0x14 + 2), W
-	RLCF	(r0x14 + 3), W
-	ADDWF	r0x01, F
-	MOVF	r0x04, W
-	ADDWFC	r0x02, F
+	CLRF	r1x04,   B
+	RLCF	(r1x14 + 2), W, B
+	RLCF	(r1x14 + 3), W, B
+	ADDWF	r1x01, F, B
+	MOVF	r1x04, W, B
+	ADDWFC	r1x02, F, B
 ;	.line	74; fsmul.c	fl1.l = MANT (fl1.l);
-	BSF	(r0x10 + 2), 7
-	CLRF	(r0x10 + 3),   A
+	BSF	(r1x10 + 2), 7, B
+	CLRF	(r1x10 + 3),   B
 ;	.line	75; fsmul.c	fl2.l = MANT (fl2.l);
-	BSF	(r0x14 + 2), 7
-	CLRF	(r0x14 + 3),   A
+	BSF	(r1x14 + 2), 7, B
+	CLRF	(r1x14 + 3),   B
 ;	.line	78; fsmul.c	result = (fl1.l >> 8) * (fl2.l >> 8);
 	CLRF	POSTDEC1,   A
-	MOVF	(r0x14 + 3), W
+	MOVF	(r1x14 + 3), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x14 + 2), W
+	MOVF	(r1x14 + 2), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x14 + 1), W
+	MOVF	(r1x14 + 1), W, B
 	MOVWF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
-	MOVF	(r0x10 + 3), W
+	MOVF	(r1x10 + 3), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x10 + 2), W
+	MOVF	(r1x10 + 2), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x10 + 1), W
+	MOVF	(r1x10 + 1), W, B
 	MOVWF	POSTDEC1,   A
 	CALL	__mullong
-	MOVWF	r0x03,   A
-	MOVFF	PRODL, r0x04
-	MOVFF	PRODH, r0x05
-	MOVFF	FSR0L, r0x06
+	BANKSEL r1x00
+	MOVWF	r1x03,   B
+	MOVFF	PRODL, r1x04
+	MOVFF	PRODH, r1x05
+	MOVFF	FSR0L, r1x06
 	MOVLW	0x08
 	ADDWF	FSR1L, F
 ;	.line	79; fsmul.c	result += ((fl1.l & (unsigned long) 0xFF) * (fl2.l >> 8)) >> 8;
 	CLRF	POSTDEC1,   A
-	MOVF	(r0x14 + 3), W
+	MOVF	(r1x14 + 3), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x14 + 2), W
+	MOVF	(r1x14 + 2), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x14 + 1), W
+	MOVF	(r1x14 + 1), W, B
 	MOVWF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
-	MOVF	r0x10, W
+	MOVF	r1x10, W, B
 	MOVWF	POSTDEC1,   A
 	CALL	__mullong
+	BANKSEL r1x00
 	MOVLW	0x08
 	ADDWF	FSR1L, F
 	MOVF	PRODL, W
-	ADDWF	r0x03, F
+	ADDWF	r1x03, F, B
 	MOVF	PRODH, W
-	ADDWFC	r0x04, F
+	ADDWFC	r1x04, F, B
 	MOVF	FSR0L, W
-	ADDWFC	r0x05, F
+	ADDWFC	r1x05, F, B
 	MOVLW	0x00
-	ADDWFC	r0x06, F
+	ADDWFC	r1x06, F, B
 ;	.line	80; fsmul.c	result += ((fl2.l & (unsigned long) 0xFF) * (fl1.l >> 8)) >> 8;
 	CLRF	POSTDEC1,   A
-	MOVF	(r0x10 + 3), W
+	MOVF	(r1x10 + 3), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x10 + 2), W
+	MOVF	(r1x10 + 2), W, B
 	MOVWF	POSTDEC1,   A
-	MOVF	(r0x10 + 1), W
+	MOVF	(r1x10 + 1), W, B
 	MOVWF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
 	CLRF	POSTDEC1,   A
-	MOVF	r0x14, W
+	MOVF	r1x14, W, B
 	MOVWF	POSTDEC1,   A
 	CALL	__mullong
+	BANKSEL r1x00
 	MOVLW	0x08
 	ADDWF	FSR1L, F
 	MOVF	PRODL, W
-	ADDWF	r0x03, F
+	ADDWF	r1x03, F, B
 	MOVF	PRODH, W
-	ADDWFC	r0x04, F
+	ADDWFC	r1x04, F, B
 	MOVF	FSR0L, W
-	ADDWFC	r0x05, F
+	ADDWFC	r1x05, F, B
 	MOVLW	0x00
-	ADDWFC	r0x06, F
+	ADDWFC	r1x06, F, B
 ;	.line	83; fsmul.c	result += 0x40;
 	MOVLW	0x40
-	ADDWF	r0x03, F
+	ADDWF	r1x03, F, B
 	MOVLW	0x00
-	ADDWFC	r0x04, F
-	ADDWFC	r0x05, F
-	ADDWFC	r0x06, F
+	ADDWFC	r1x04, F, B
+	ADDWFC	r1x05, F, B
+	ADDWFC	r1x06, F, B
 ;	.line	85; fsmul.c	if (result & SIGNBIT)
-	BTFSS	r0x06, 7
+	BTFSS	r1x06, 7, B
 	BRA	_00109_DS_
 ;	.line	88; fsmul.c	result += 0x40;
 	MOVLW	0x40
-	ADDWF	r0x03, F
+	ADDWF	r1x03, F, B
 	MOVLW	0x00
-	ADDWFC	r0x04, F
-	ADDWFC	r0x05, F
-	ADDWFC	r0x06, F
+	ADDWFC	r1x04, F, B
+	ADDWFC	r1x05, F, B
+	ADDWFC	r1x06, F, B
 ;	.line	89; fsmul.c	result >>= 8;
-	MOVF	r0x04, W
-	MOVWF	r0x03,   A
-	MOVF	r0x05, W
-	MOVWF	r0x04,   A
-	MOVF	r0x06, W
-	MOVWF	r0x05,   A
-	CLRF	r0x06,   A
+	MOVF	r1x04, W, B
+	MOVWF	r1x03,   B
+	MOVF	r1x05, W, B
+	MOVWF	r1x04,   B
+	MOVF	r1x06, W, B
+	MOVWF	r1x05,   B
+	CLRF	r1x06,   B
 	BRA	_00110_DS_
 _00109_DS_:
 ;	.line	93; fsmul.c	result >>= 7;
-	RLCF	r0x03, W
-	RLCF	r0x04, W
-	MOVWF	r0x03,   A
-	CLRF	r0x04,   A
-	RLCF	r0x04, F
-	MOVF	r0x05, W
+	RLCF	r1x03, W, B
+	RLCF	r1x04, W, B
+	MOVWF	r1x03,   B
+	CLRF	r1x04,   B
+	RLCF	r1x04, F, B
+	MOVF	r1x05, W, B
 	RLNCF	WREG, W
 	ANDLW	0xfe
-	IORWF	r0x04, F
-	RLCF	r0x05, W
-	RLCF	r0x06, W
-	MOVWF	r0x05,   A
-	CLRF	r0x06,   A
-	RLCF	r0x06, F
+	IORWF	r1x04, F, B
+	RLCF	r1x05, W, B
+	RLCF	r1x06, W, B
+	MOVWF	r1x05,   B
+	CLRF	r1x06,   B
+	RLCF	r1x06, F, B
 ;	.line	94; fsmul.c	--exp;
 	MOVLW	0xff
-	ADDWF	r0x01, F
-	ADDWFC	r0x02, F
+	ADDWF	r1x01, F, B
+	ADDWFC	r1x02, F, B
 _00110_DS_:
 ;	.line	97; fsmul.c	result &= ~HIDDEN;
-	BCF	r0x05, 7
+	BCF	r1x05, 7, B
 ;	.line	100; fsmul.c	if (exp >= 0x100)
-	MOVF	r0x02, W
+	MOVF	r1x02, W, B
 	ADDLW	0x80
 	ADDLW	0x7f
 	BNZ	_00152_DS_
 	MOVLW	0x00
-	SUBWF	r0x01, W
+	SUBWF	r1x01, W, B
 _00152_DS_:
 	BNC	_00115_DS_
 ;	.line	101; fsmul.c	fl1.l = (sign ? SIGNBIT : 0) | 0x7F800000;
-	MOVF	r0x00, W
+	MOVF	r1x00, W, B
 	BZ	_00119_DS_
 ;	.line	106; fsmul.c	return fl1.f;
 	MOVLW	0xFF
@@ -299,7 +291,7 @@ _00119_DS_:
 _00115_DS_:
 ;	.line	102; fsmul.c	else if (exp < 0)
 	BSF	STATUS, 0
-	BTFSS	r0x02, 7
+	BTFSS	r1x02, 7, B
 	BCF	STATUS, 0
 	BNC	_00112_DS_
 ;	.line	103; fsmul.c	fl1.l = 0;
@@ -311,66 +303,51 @@ _00115_DS_:
 	BRA	_00117_DS_
 _00112_DS_:
 ;	.line	105; fsmul.c	fl1.l = PACK (sign ? SIGNBIT : 0 , exp, result);
-	MOVF	r0x00, W
+	MOVF	r1x00, W, B
 	BZ	_00121_DS_
-;	CLRF	r0x00,   A
-;	CLRF	r0x07,   A
-;	CLRF	r0x08,   A
+;	CLRF	r1x00,   B
+;	CLRF	r1x07,   B
+;	CLRF	r1x08,   B
 	MOVLW	0x80
-	MOVWF	r0x10,   A
+	MOVWF	r1x10,   B
 	BRA	_00122_DS_
 _00121_DS_:
-;	CLRF	r0x00,   A
-;	CLRF	r0x07,   A
-;	CLRF	r0x08,   A
-	CLRF	r0x10,   A
+;	CLRF	r1x00,   B
+;	CLRF	r1x07,   B
+;	CLRF	r1x08,   B
+	CLRF	r1x10,   B
 _00122_DS_:
 	CLRF	WREG,   A
-	RRCF	r0x02, W
-	RRCF	r0x01, W
-	MOVWF	r0x12,   A
-	CLRF	r0x11,   A
-	RRCF	r0x11, F
+	RRCF	r1x02, W, B
+	RRCF	r1x01, W, B
+	MOVWF	r1x12,   B
+	CLRF	r1x11,   B
+	RRCF	r1x11, F, B
 ;	MOVLW	0x00
-;	IORWF	r0x00, F
-;	IORWF	r0x07, F
-;	MOVF	r0x11, W
-;	IORWF	r0x08, F
-	MOVF	r0x12, W
-	IORWF	r0x10, F
-;	MOVF	r0x00, W
-;	IORWF	r0x03, F
-;	MOVF	r0x07, W
-;	IORWF	r0x04, F
-;	MOVF	r0x08, W
-	MOVF	r0x11, W
-	IORWF	r0x05, F
-	MOVF	r0x10, W
-	IORWF	r0x06, F
+;	IORWF	r1x00, F, B
+;	IORWF	r1x07, F, B
+;	MOVF	r1x11, W, B
+;	IORWF	r1x08, F, B
+	MOVF	r1x12, W, B
+	IORWF	r1x10, F, B
+;	MOVF	r1x00, W, B
+;	IORWF	r1x03, F, B
+;	MOVF	r1x07, W, B
+;	IORWF	r1x04, F, B
+;	MOVF	r1x08, W, B
+	MOVF	r1x11, W, B
+	IORWF	r1x05, F, B
+	MOVF	r1x10, W, B
+	IORWF	r1x06, F, B
 ;	.line	106; fsmul.c	return fl1.f;
-	MOVF	r0x06, W
+	MOVF	r1x06, W, B
 	MOVWF	FSR0L,   A
-	MOVF	r0x05, W
+	MOVF	r1x05, W, B
 	MOVWF	PRODH,   A
-	MOVF	r0x04, W
+	MOVF	r1x04, W, B
 	MOVWF	PRODL,   A
-	MOVF	r0x03, W
+	MOVF	r1x03, W, B
 _00117_DS_:
-	MOVFF	PREINC1, r0x17
-	MOVFF	PREINC1, r0x16
-	MOVFF	PREINC1, r0x15
-	MOVFF	PREINC1, r0x14
-	MOVFF	PREINC1, r0x13
-	MOVFF	PREINC1, r0x12
-	MOVFF	PREINC1, r0x11
-	MOVFF	PREINC1, r0x10
-	MOVFF	PREINC1, r0x06
-	MOVFF	PREINC1, r0x05
-	MOVFF	PREINC1, r0x04
-	MOVFF	PREINC1, r0x03
-	MOVFF	PREINC1, r0x02
-	MOVFF	PREINC1, r0x01
-	MOVFF	PREINC1, r0x00
 	MOVFF	PREINC1, FSR2L
 	RETURN	
 
